a4a20047a4b33dd6837483ce6f5710be0056824e
ginapult <gina.pultorak@gmail.com>
2019-09-09 16:12:09 -0700

    remove extraneous folders

--- middleware/error.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = (error, reques, response, next) => {
-  console.log('__SERVER_ERROR__', error);
-  let err = { error: error.message || error };
-  response.status(500).json(error);
-};
