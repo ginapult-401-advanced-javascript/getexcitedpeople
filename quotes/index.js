8ce8808b9811c13e0dcb1b39f9c781540eb2142f
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-09-09 14:20:42 -0700

    fleshing out database

--- quotes/index.js
+++ quotes/index.js
@@ -5,51 +5,61 @@ let quotes = [
     id:1,
     quote:'Do one thing every day that scares you',
     author: 'Eleanor Roosevelt',
+    img_url: 'https://i.ytimg.com/vi/-pYuSSP5Wls/maxresdefault.jpg',
   },
   {
     id:2,
     quote:'We are what we pretend to be, so we must be careful about what we pretend to be.',
     author: 'Kurt Vonnegut',
+    img_url: 'http://www.kickvick.com/wp-content/uploads/2014/11/cute-baby-animals-a-36.jpg',
   },
   {
     id:3,
     quote:'When we love, we always strive to become better than we are. When we strive to become better than we are, everything around us becomes better too.',
     author: 'Paulo Coelho',
+    img_url: 'https://i.ytimg.com/vi/eQb5-kcnx18/maxresdefault.jpg',
   },
   {
     id:4,
     quote:'Sometimes you wake up. Sometimes the fall kills you. And sometimes, when you fall, you fly.',
     author:' Neil Gaiman',
+    img_url: 'https://i.pinimg.com/736x/be/31/a3/be31a3f4d1a3bd83fd395dec6b9b012a.jpg',
   },
   {
     id:5,
     quote:'And now here is my secret, a very simple secret: It is only with the heart that one can see rightly; what is essential is invisible to the eye.',
     author: 'Antoine de Saint-Exupéry',
+    img_url: 'https://s-i.huffpost.com/gen/2661384/images/o-AWE-facebook.jpg',
   },
   {
     id:6,
     quote:'What\'s meant to be will always find a way',
     author: 'Trisha Yearwood',
+    img_url: 'http://i1.ytimg.com/vi/AnYluZ8UpsA/hqdefault.jpg',
   },
   {
     id:7,
     quote:'The flower that blooms in adversity is the rarest and most beautiful of all.',
     author: 'Walt Disney Company',
+    img_url: 'https://i.ytimg.com/vi/lB4SvxKgpAU/maxresdefault.jpg'
   },
   {
     id:8,
     quote:'We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.',
     author: 'Maya Angelou',
+    img_url: 'https://www.hdwallpapers.in/walls/puppy_love-normal.jpg',
   },
   {
     id:9,
     quote:'You never have to change anything you got up in the middle of the night to write.',
     author: 'Saul Bellow',
+    img_url: '',
   },
   {
     id:10,
     quote:'Courage is the most important of all the virtues because without courage, you can\'t practice any other virtue consistently.',
     author: 'Maya Angelou',
+    img_url: 'https://amazinganimalphotos.com/wp-content/uploads/2013/04/cute-kitten-playing-with-bubbles-photo.png'
   },
   {
     id:11,
@@ -60,16 +70,18 @@ let quotes = [
     id:12,
     quote:'I was never really insane except upon occasions when my heart was touched.',
     author: 'Edgar Allan Poe',
+    img_url: 'http://2.bp.blogspot.com/-BRFnOq3PSrY/UTy8m0DAthI/AAAAAAAAAdo/Pj0iA4Lm0Ew/s1600/spring-time.jpg',
   },
   {
     id:13,
     quote:'Pain is temporary. Quitting lasts forever.',
-    author: ' Lance Armstrong Sally Jenkins',
+    author: 'Lance Armstrong Sally Jenkins',
   },
   {
     id:14,
     quote:'If someone starts to speak unkindly to me, I will remember that they have got something going on that has nothing to do with me. Like maybe they just pooped their pants. Yes, that\'s probably it',
     author: 'Knock Knock',
+    img_url: 'https://www.wired.com/wp-content/uploads/2015/04/85120553.jpg',
   },
   {
     id:15,
@@ -80,26 +92,31 @@ let quotes = [
     id:16,
     quote:'',
     author: 'Knock Knock',
+    img_url: 'https://tracts4free.files.wordpress.com/2015/01/dsc_0155-little-finger-baby-ndt.jpg',
   },
   {
     id:17,
     quote:'',
     author: 'Knock Knock',
+    img_url: '',
   },
   {
     id:18,
     quote:'',
     author: 'Knock Knock',
+    img_url: 'https://tpzoo.files.wordpress.com/2014/01/ducklings.jpg',
   },
   {
     id:19,
     quote:'',
     author: 'Knock Knock',
+    img_url: '',
   },
   {
     id:20,
     quote:'',
     author: 'Knock Knock',
+    img_url: 'https://c2.staticflickr.com/6/5216/5491756636_17818b05a1_z.jpg',
   },
   {
     id:21,
@@ -110,6 +127,7 @@ let quotes = [
     id:22,
     quote:'',
     author: 'Knock Knock',
+    img_url: '',
   },
   {
     id:23,
