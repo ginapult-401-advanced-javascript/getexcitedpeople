507d51114355bcff867157246b9173176cfc6470
LYNDSEY THOMAS <THOMAS.LYNDSEY@GMAIL.COM>
2019-09-10 13:32:01 -0700

    lint errors

--- __tests__/generator.test.js
+++ __tests__/generator.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let quotesLibrary = require('../src/quotes/data.js');
-let generatedQuotes = require('../src/quotes/generator.js')
+let generatedQuotes = require('../src/quotes/generator.js');
 
 describe('can it randomly generate a new number/quote each time it is called', () => {
   test('can generate a random quote', () => {
