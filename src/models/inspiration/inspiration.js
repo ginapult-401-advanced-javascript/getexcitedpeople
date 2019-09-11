b821f4fd942ac195b1844e60db6e4f358d6e1029
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-11 13:49:57 -0700

    Fix model instantiation and comment autoIncrement WIP

--- src/models/inspiration/inspiration.js
+++ src/models/inspiration/inspiration.js
@@ -4,32 +4,34 @@ const Model = require('../Model.js');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const autoIncrement = require('mongoose-auto-increment');
+// const autoIncrement = require('mongoose-auto-increment');
 
-const connection = mongoose.createConnection(process.env.MONGODB_URI);
+// const connection = mongoose.createConnection(process.env.MONGODB_URI);
 
-autoIncrement.initialize(connection);
+// autoIncrement.initialize(connection);
 
 const inspirationSchema = mongoose.Schema({
   user_id: { type:String, required: true },
   // TODO: User readible content_id (auto-increment, unique)
-  display_id: { type:Number, unique:true, ref: 'display_id' },
+  // display_id: { type:Number, unique:true, ref: 'display_id' },
   content: { type: String, required: true },
   type: { type: String },
 });
+
+const inspirationMongooseModel = mongoose.model('inspirationSchema', inspirationSchema);
 
 // inspirationSchema.pre('save',function(){
 //   display_id =
 // });
 
-inspirationSchema.plugin(autoIncrement.plugin, 'display_id');
+// inspirationSchema.plugin(autoIncrement.plugin, 'display_id');
 
 /**
  * Class representing a Content Item.
  * @extends Model
  */
 class Inspiration extends Model {
-  constructor() { super(inspirationSchema); }
+  constructor() { super(inspirationMongooseModel); }
 
 }
 module.exports = Inspiration;
