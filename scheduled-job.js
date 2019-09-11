55b5895742d22711d8a07fc7b2a279914587ceda
ginapult <gina.pultorak@gmail.com>
2019-09-11 11:17:32 -0700

    pull random message and send to bot

--- scheduled-job.js
+++ scheduled-job.js
@@ -4,6 +4,15 @@ const slackBot = require('./src/slack/api.js');
 
 const channelName = 'bot-testing';
 
+function generateScheduleInspiration() {
+    return inspiration.get()
+      .then(allContent => {
+        const randomIndex = Math.floor(Math.random() * allContent.length);
+        return allContent[randomIndex];
+      })
+      .catch(console.error);
+};
+
 function sendScheduledInspiration() {
     slackBot.sendMessage(channelName, 'BE INSPIRED REGULARLY!!!!')
     console.log('scheduled inspiration triggered');
