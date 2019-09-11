7901f9263f0492d3f608fff4f091cc513e9ed016
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-10 17:23:37 -0700

    Update Content schema

--- src/models/content/content-schema.js
+++ src/models/content/content-schema.js
@@ -3,27 +3,11 @@
 const mongoose = require('mongoose');
 
 const contentSchema = mongoose.Schema({
-  user_id: {type:String, required: true, unique:true },
+  user_id: { type:String, required: true },
+  // TODO: User readible content_id (auto-increment, unique)
+  // content_id: { type:String, required: true, unique:true },
   content: { type: String, required: true },
-  type: {type: String, required: true },
+  type: { type: String },
 });
-
-contentSchema.statics.getInspiration = function (userId) {
-  let contents = require('../../content/content-library').contents;
-  let count = contents.length; 
-  let randomId = Math.floor((Math.random()* count) + 1);
-  let result = {};
-  contents.forEach(content => {
-    if(content.content_id === randomId){
-      result = content;
-    }
-  });
-  return result;
-};
-
-
-
-
-
 
 module.exports = mongoose.model('contentSchema', contentSchema);
