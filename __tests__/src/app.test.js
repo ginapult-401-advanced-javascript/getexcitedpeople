936c81cfb00f87cb1fff3e486da8a903686253da
Austin Hedeen <austin.hedeen@gmail.com>
2019-09-12 09:58:53 -0700

    Add 404 routing test

---
+++ __tests__/src/app.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const rootDir = process.cwd();
+const supergoose = require('./supergoose.js');
+const {server} = require(`${rootDir}/src/app.js`);
+const mockRequest = supergoose.server(server);
+
+beforeAll(supergoose.startDB);
+afterAll(supergoose.stopDB);
+
+describe('Test Server Error Response', () => {
+
+  it('should respond with a 404 on an invalid route', () => {
+
+    return mockRequest
+      .get('/foo')
+      .then(results => {
+        expect(results.status).toBe(404);
+      });
+
+  });
+
+});
